a5731a790bcf1e74bf6cdbdd47c91a09326c94ad
ChaosKyle <kyle@ksbdigital.com>
2021-04-25 14:54:09 -0500

    Update App.js
    Creating upload button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-import Amplify from 'aws-amplify';
+
+// Amplify libraries
+import Amplify, { Storage } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
 import awsconfig from './aws-exports';
-import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
 Amplify.configure(awsconfig);
-function App() {
+
+const App = () => {
+  const [imageUrl, setImageUrl] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const downloadUrl = async () => {
+    const downloadUrl = await Storage.get('picture.jpg', { level: 'private', expires: 10 });
+    window.location.href = downloadUrl
+  }
+
+  const handleChange = async (e) => {
+    const file = e.target.files[0];
+    try {
+      setLoading(true);
+      await Storage.put('picture.jpg', file, {
+        level: 'private',
+        contentType: 'image/jpg'
+      });
+      const url = await Storage.get('picture.jpg', { level: 'private' })
+      setImageUrl(url);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-      	<AmplifySignOut />
-	<h2>GoatDiag</h2>      
-	</header>
+      <h1> Upload an Image </h1>
+      {loading ? <h3>Uploading...</h3> : <input
+        type="file" accept='image/jpg'
+        onChange={(evt) => handleChange(evt)}
+      />}
+      <div>
+        {imageUrl ? <img style={{ width: "30rem" }} src={imageUrl} /> : <span />}
+      </div>
+      <div>
+        <h2>Download URL?</h2>
+        <button onClick={() => downloadUrl()}>Click Here!</button>
+      </div>
     </div>
   );
 }
