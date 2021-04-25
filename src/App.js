1d3cbbc467046314db5c2facbebe7c86e6ca42f4
ChaosKyle <kyle@ksbdigital.com>
2021-04-25 11:58:48 -0500

    Adding S3 bucket/Frontend TEst
    amplify add storage
    fuckin around with the frontend to start building the file upload logic

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <header className="App-header">
       	<AmplifySignOut />
-	<h2>My App Content</h2>      
+	<h2>GOATDIAG</h2>
 	</header>
     </div>
   );
